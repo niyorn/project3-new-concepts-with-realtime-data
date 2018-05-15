b75e82411bd407566b1a1b2d4df1ce263dc80295
niyorn <c.niyornram@hotmail.com>
2018-05-15 07:32:42 +0200

    display content to html

--- main.js
+++ main.js
@@ -1,15 +1,51 @@
-const express = require('express')
-    app = express ();
-const router = require('./routes/routes')
+const express = require('express');
+const app = express();
+const router = require('./routes/routes');
 const port = 4000;
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const socket = require('socket.io');
+const csvParse = require('csv-parse');
+const dataPath = 'data/data.json';
+const fs = require('fs');
+const csv = require('csvtojson');
+
 
 app
     .use(express.static('views'))
     .use(express.static('public'))
-    .set('view engine', 'ejs')
+    .set('view engine', 'ejs');
 
-app.use('/', router)
+app.use('/', router);
 
-app.listen(port, function () {
+//Get data from a file
+let data = JSON.parse(fs.readFileSync(dataPath, 'utf8'));
+console.log(data)
+//Map the data with only the content we need
+let dataMap = data.map((i) => {
+    let content = {};
+    content.id = i['entry_id'];
+    content.temperature = i['Room Temp (*C)'];
+    content.time = i[""];
+    return content;
+})
+
+
+
+io.on('connection', function (socket) {
+
+    for (let i = 0; i < dataMap.length; i++) {
+        (function (i) {
+            setTimeout(function () {
+                socket.emit('hydroponics', dataMap[i])
+            }, 1000 * i);
+        })(i);
+    };
+
+})
+
+
+
+http.listen(port, function () {
     console.log('open at localhost:' + port)
 })
